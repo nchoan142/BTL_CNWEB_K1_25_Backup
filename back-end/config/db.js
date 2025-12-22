b31ed6567b202a4413a4970f568366f044ceec52
nchoan142 <nchoan142@gmail.com>
2025-12-22 12:01:34 +0700

    sua file

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,11 +1,11 @@
-// server/config/db.js
+// db.js chưasp nối đến database MySQL
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',        // Điền password MySQL của bạn (nếu có)
-    database: 'hotel_db' // Tên database bạn đã tạo
+    password: '123456',        
+    database: 'hotel_db' // Tên database đã tạo
 });
 
 module.exports = pool.promise();
--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,15 +1,24 @@
 // Quản lý đăng nhập, đăng ký
 
+// Express giúp tạo Server và định nghĩa các Route
+// Các Routers đóng vai trò là Controller trong MVC
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db'); // Lưu ý dấu .. để lùi ra thư mục cha
+// Kết nối database
+const db = require('../config/db');
 
 // Đăng ký (URL: /api/auth/register)
 router.post('/register', async (req, res) => {
+    // const { username, password, full_name, email, phone } = req.body
+    // Lấy dữ liệu từ Front-end
+    // Sử dụng destructuring để lấy các trường cần thiết
     const { username, password, full_name, email, phone } = req.body;
     try {
         const sql = `INSERT INTO users (username, password, full_name, email, phone) VALUES (?, ?, ?, ?, ?)`;
-        await db.query(sql, [username, password, full_name, email, phone]);
+        // db.query(sql, [username, password, full_name, email, phone])
+        // Thực thi câu lệnh SQL với các giá trị tương ứng
+        // Có bao nhiêu dấu ? thì truyền bấy nhiêu giá trị trong mảng
+        await db.query(sql, [username, password, full_name, email, phone]); // Sử dụng promise/ async-await
         res.status(201).json({ success: true, message: "Đăng ký thành công!" });
     } catch (error) {
         res.status(400).json({ success: false, error: "Username hoặc Email đã tồn tại" });
